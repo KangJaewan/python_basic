6f7c2d52aaaf2f0edb67b309696b33e4a0d2b3a5
Jaewan <wanaje@pukyong.ac.kr>
2025-03-11 13:46:43 +0900

    딕셔너리 연습

---
+++ 07_딕셔너리.py
@@ -0,0 +1,8 @@
+name_card = {'name' : '홍길동',
+             'tel' : '010-888-9999',
+             'address' : '부산',
+             'email' : 'hong@gmail.com'}
+
+print(name_card)
+
+name_card['etc'] = ['이사갈수도 있음...','1년안에']
